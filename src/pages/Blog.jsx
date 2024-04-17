d555a932080aa19c0a0f67bb52f6edfb50de6891
ch1nru5t <chinrust@gmail.com>
2024-04-17 17:36:00 +0530

    fix blog event

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -117,7 +117,7 @@ export default function Blog() {
               <BlogItem
                 onClick={() => {
                   mixpanel.track("Blog Clicked", {
-                    "Blog Title": blog.title,
+                    BlogTitle: blog.title,
                     Category: blog.category,
                     Date: blog.date,
                     Comments: blog.comments,
