0b1234fe1c3f7691d9a336bafab36455bdab1304
Iterate-id <support@iterate.ai.com>
2024-06-28 16:03:30 +0530

    Apply diff to multiple files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -114,7 +114,7 @@ export default function Header() {
                           </Link>
                         </li>
                         <li>
-                          <Link to={ROUTES.STORES}>Stores</Link>
+                          <Link to={ROUTES.STORES} onClick={() => mixpanel.track('nav_link_clicked', { rank: 2 })}>Stores</Link>
                         </li>
                         <li>
                           <Link to={ROUTES.CONTACT}>Contacts</Link>
